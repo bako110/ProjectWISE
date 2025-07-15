c6d55363842d867ef140b8e9c8efed9c6fd4b1e2
bako110 <bakorobert2000@gmail.com>
2025-07-15 14:30:09 +0200

    changement du nom User par user.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     console.log('MONGO_URI:', process.env.MONGO_URI);
-    await mongoose.connect(process.env.MONGO_URI);
+    // await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB connecté avec succès');
   } catch (error) {
     console.error('❌ Échec de la connexion à MongoDB:', error);
