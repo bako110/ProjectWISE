fea8fca615f9c72d1320ad213ee98f668f3fa15f
bako110 <bakorobert2000@gmail.com>
2025-07-15 15:56:32 +0200

    changement du nom User par user.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import profileRoutes from './routes/profile.js';
 import swaggerDocs from './swagger.js'; 
+import publicIp from 'public-ip';  // <-- import public-ip
 
 // Configurations
 dotenv.config();
@@ -11,6 +12,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 swaggerDocs(app);
+
 // Import des routes
 import authRoutes from './routes/authRoutes.js';
 import collectorRoutes from './routes/collectorRoutes.js'; 
@@ -20,6 +22,15 @@ app.use('/api/auth', authRoutes);
 app.use('/api/collectors', collectorRoutes);  
 app.use('/api', profileRoutes);
 
+// Nouvelle route pour afficher l'IP publique de Render
+app.get('/myip', async (req, res) => {
+  try {
+    const ip = await publicIp.v4();  // récupère IP publique IPv4
+    res.send(`IP publique de ce serveur : ${ip}`);
+  } catch (error) {
+    res.status(500).send('Impossible de récupérer l\'IP publique');
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 
