48ea709e452c7fe50ceca523f7d0c7b509e12104
bako110 <bakorobert2000@gmail.com>
2025-07-18 13:01:57 +0200

    Ajout de core

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'; // <--- ⬅️ AJOUT ICI
 import connectDB from './config/db.js';
 import swaggerDocs from './swagger.js';
-
 
 // Import des routes
 import authRoutes from './routes/authRoutes.js';
@@ -10,15 +10,18 @@ import collectorRoutes from './routes/collectors/collectorRoutes.js';
 import profileRoutes from './routes/profile.js';
 import zoneRoutes from './routes/agency/zoneRoutes.js';  
 import planningRoutes from './routes/agency/planningRoutes.js';
-import agencyClientRoutes from './routes/agency/clientRoutes.js' ;
-import clientRoutes from './routes/clients/clientRoutes.js'
-import agenceSearchRoutes from './routes/clients/agencySearchRoutes.js'
+import agencyClientRoutes from './routes/agency/clientRoutes.js';
+import clientRoutes from './routes/clients/clientRoutes.js';
+import agenceSearchRoutes from './routes/clients/agencySearchRoutes.js';
 
-// Configurations et connexion à la base
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// ✅ Middleware CORS (autoriser toutes les origines, ou configurer)
+app.use(cors({ origin: 'http://localhost:4200' }));
+
 
 // Middlewares globaux
 app.use(express.json());
@@ -29,13 +32,12 @@ swaggerDocs(app);
 // Déclaration des routes
 app.use('/api/auth', authRoutes);
 app.use('/api/collectors', collectorRoutes);
-app.use('/api/zones', zoneRoutes);   // <-- route Zones
+app.use('/api/zones', zoneRoutes);
 app.use('/api', profileRoutes);
 app.use('/api/plannings', planningRoutes);
 app.use('/api/agences', agenceSearchRoutes);
 app.use('/api', agencyClientRoutes);
 app.use('/api/clients', clientRoutes);
-
 
 // Middleware 404 pour routes non trouvées
 app.use((req, res) => {
