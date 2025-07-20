193f70f4839a53c765deb7803c96c8ef6ef13394
bako110 <bakorobert2000@gmail.com>
2025-07-20 02:01:16 +0200

    Souscription a une agence

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import municipalityRoutes from './routes/mairies/municipalityRoutes.js';
 import zoneRoutes from './routes/agency/zoneRoutes.js';
 import profileRoutes from './routes/profile.js';
 import agenceSearchRoutes from './routes/clients/agencySearchRoutes.js'
+import agencyClientRoutes from './routes/clients/clientRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -37,7 +38,7 @@ app.use('/api/zones', zoneRoutes);
 app.use('/api', profileRoutes);
 // app.use('/api/plannings', planningRoutes);
 app.use('/api/agences', agenceSearchRoutes);
-// app.use('/api', agencyClientRoutes);
+app.use('/api/clients', agencyClientRoutes);
 // app.use('/api/clients', clientRoutes);
 
 // Middleware 404 pour routes non trouvées
