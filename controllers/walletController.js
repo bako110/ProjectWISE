b7edc3e0e3d8b4d225b9460cf6210063afbd3d23
papa-salif <salif.o@sahelys.com>
2025-09-24 12:09:10 +0000

    amount

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -40,6 +40,9 @@ export const addFunds = async (req, res) => {
     if (!wallet) {
       return res.status(404).json({ error: "Wallet not found for this user" });
     }
+    if ( amount <= 0) {
+      return res.status(400).json({ error: "Amount must be a positive number" });
+    }
     wallet.balance += amount;
     await wallet.save();
     res.status(200).json({message: "Funds added successfully", wallet});
