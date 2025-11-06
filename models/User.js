0e17ce1e4b3a66328656927b32559eccba6a615c
bako110 <bakorobert2000@gmail.com>
2025-11-06 12:06:20 +0100

    gestion des tarifs

--- models/User.js
+++ models/User.js
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema({
     status: {
     type: String,
     enum: ['active', 'inactive', 'deleted'],
-    default: 'active',
+    default: 'inactive',
   },
   acceptTerms: {
     type: Boolean,
