4764bae73176ef491caf8ff9156bd235074c228e
bako110 <bakorobert2000@gmail.com>
2025-11-06 16:48:03 +0100

    Insertion du wallet

---
+++ models/wallet.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const walletSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+    balance: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+    kind: {
+    type: String,
+    enum: ['standard', 'premium'],
+    default: 'standard',
+    required: true
+  },
+}, { timestamps: true });
+
+const Wallet = mongoose.model('Wallet', walletSchema);
+export default Wallet;
